96311dd8a63af68d2f82097788d009f696a33f13
Valeriy Zhirnov <neonailol@gmail.com>
2018-01-19 19:07:16 +0300

    Main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "nnl.rocks.projects"
 version = "0.1-SNAPSHOT"
+
+buildscript {
+    repositories {
+        mavenCentral()
+        jcenter()
+        maven("https://jitpack.io")
+        maven("https://repo.spring.io/milestone")
+    }
+
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:1.5.9.RELEASE")
+    }
+}
 
 repositories {
     mavenCentral()
@@ -10,8 +24,35 @@ repositories {
 
 plugins {
     kotlin("jvm") version "1.2.20"
+    kotlin("plugin.spring") version "1.2.20"
+    id("io.spring.dependency-management") version "1.0.3.RELEASE"
+}
+
+apply {
+    plugin("org.springframework.boot")
 }
 
 dependencies {
+
+    compile("com.github.neonailol.kactoos:kactoos-jvm:master-SNAPSHOT")
+
     compile(kotlin("stdlib"))
+    compile(kotlin("reflect"))
+
+    compile("org.springframework.boot:spring-boot-starter-validation")
+    compile("org.springframework.boot:spring-boot-starter-web")
+
+    compile("com.h2database:h2")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    testCompile("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+    }
 }
---
+++ src/main/kotlin/nnl/rocks/projects/spkro/Application.kt
@@ -0,0 +1,12 @@
+package nnl.rocks.projects.spkro
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
