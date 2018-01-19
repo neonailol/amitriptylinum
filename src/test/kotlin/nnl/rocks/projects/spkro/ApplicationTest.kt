b2e9f6b0915c0668ee376c7b1205406261656d3d
Valeriy Zhirnov <neonailol@gmail.com>
2018-01-20 00:37:12 +0300

    Better test for GetPetTypesResource

--- src/test/kotlin/nnl/rocks/projects/spkro/ApplicationTest.kt
+++ src/test/kotlin/nnl/rocks/projects/spkro/ApplicationTest.kt
@@ -1,18 +1,42 @@
 package nnl.rocks.projects.spkro
 
+import nnl.rocks.projects.spkro.pets.PetTypeEntity
+import nnl.rocks.projects.spkro.pets.PetTypeEntityRepository
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import java.util.UUID
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
 @WebAppConfiguration
+@AutoConfigureMockMvc
+@SpringBootTest(classes = [Application::class], webEnvironment = RANDOM_PORT)
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 class ApplicationTest {
+
+    @Autowired
+    internal lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var petTypeEntityRepository: PetTypeEntityRepository
 
     @Test
     fun contextLoads() {
+    }
 
+    @Before
+    fun init() {
+        petTypeEntityRepository.save(PetTypeEntity(UUID.randomUUID(), "Type1"))
+        petTypeEntityRepository.save(PetTypeEntity(UUID.randomUUID(), "Type2"))
+        petTypeEntityRepository.save(PetTypeEntity(UUID.randomUUID(), "Type3"))
     }
 }
--- src/test/kotlin/nnl/rocks/projects/spkro/pets/GetPetTypesResourceTest.kt
+++ src/test/kotlin/nnl/rocks/projects/spkro/pets/GetPetTypesResourceTest.kt
@@ -1,43 +1,18 @@
 package nnl.rocks.projects.spkro.pets
 
-import nnl.rocks.projects.spkro.Application
-import org.junit.Before
+import nnl.rocks.projects.spkro.ApplicationTest
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import java.util.UUID
 
-@RunWith(SpringRunner::class)
-@SpringBootTest(classes = [Application::class])
-@AutoConfigureMockMvc
-class GetPetTypesResourceTest {
-
-    @Autowired
-    private lateinit var petTypeEntityRepository: PetTypeEntityRepository
-
-    @Autowired
-    private lateinit var mockMvc: MockMvc
-
-    @Before
-    fun init() {
-        petTypeEntityRepository.save(PetTypeEntity(UUID.randomUUID(), "Type1"))
-        petTypeEntityRepository.save(PetTypeEntity(UUID.randomUUID(), "Type2"))
-        petTypeEntityRepository.save(PetTypeEntity(UUID.randomUUID(), "Type3"))
-    }
+class GetPetTypesResourceTest : ApplicationTest() {
 
     @Test
-    @Throws(Exception::class)
     fun testGetPetTypeSuccess() {
-        this.mockMvc.perform(
+        mockMvc.perform(
             get("/api/v1/pets/types").accept(MediaType.APPLICATION_JSON_VALUE)
         ).andExpect(
             status().isOk
