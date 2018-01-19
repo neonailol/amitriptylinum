0f571a739cea13c2a33482460d88914073fd8e11
Valeriy Zhirnov <neonailol@gmail.com>
2018-01-19 20:38:09 +0300

    Stubs for packages

--- src/main/kotlin/nnl/rocks/projects/spkro/pets/internal/Repositories.kt
+++ src/main/kotlin/nnl/rocks/projects/spkro/pets/internal/Repositories.kt
@@ -1,1 +1,9 @@
 package nnl.rocks.projects.spkro.pets
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface PetEntityRepository : JpaRepository<PetEntity, UUID>, JpaSpecificationExecutor<PetEntity>
