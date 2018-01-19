fb8cd5da79f42d2d59c19b32954c7de084a41db1
Valeriy Zhirnov <neonailol@gmail.com>
2018-01-19 12:55:24 +0300

    Jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,17 @@
 
+group = "nnl.rocks.projects"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven("https://jitpack.io")
+}
+
 plugins {
     kotlin("jvm") version "1.2.20"
 }
 
 dependencies {
     compile(kotlin("stdlib"))
-}
-
-repositories {
-    jcenter()
 }
